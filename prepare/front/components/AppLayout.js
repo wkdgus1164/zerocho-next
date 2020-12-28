0096d1f2046060f6411839445f8f80e2fe70840a
Han Jang Hyun <wkdgus116490@gmail.com>
2020-12-28 23:46:39 +0900

    LoginForm

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from "next/link";
-import {Menu} from "antd";
+import {Col, Input, Menu, Row} from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 
 const AppLayout = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>
             <Menu mode="horizontal">
@@ -14,10 +17,23 @@ const AppLayout = ({children}) => {
                     <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
                 <Menu.Item>
+                    <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
+                </Menu.Item>
+                <Menu.Item>
                     <Link href="/signup"><a>회원가입</a></Link>
                 </Menu.Item>
             </Menu>
-            {children}
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile/> : <LoginForm/>}
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://zerocho.com" target="_blank" rel="noreferrer noopener">Made by wkdgus1164</a>
+                </Col>
+            </Row>
         </div>
     );
 };
