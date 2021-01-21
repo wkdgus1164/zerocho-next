5b4cef04347ad132589a96d44d42c1233cd10895
Han Jang Hyun <wkdgus116490@gmail.com>
2021-01-22 00:08:35 +0900

    redux-thunk

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -2,9 +2,15 @@ import {createWrapper} from "next-redux-wrapper";
 import {applyMiddleware, compose, createStore} from "redux";
 import rootReducer from "../reducers";
 import {composeWithDevTools} from "redux-devtools-extension";
+import thunkMiddleware from 'redux-thunk';
+
+const loggerMiddleware = ({dispatch, getState}) => next => action => {
+    console.log(action);
+    return next(action);
+}
 
 const configureStore = () => {
-    const middlewares = [];
+    const middlewares = [thunkMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production' ? compose(applyMiddleware(...middlewares)) : composeWithDevTools(applyMiddleware(...middlewares));
     const store = createStore(rootReducer, enhancer);
     store.dispatch({
