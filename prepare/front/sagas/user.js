5cd849746559115b80a9e166f7bd00f51869aa6f
Han Jang Hyun <wkdgus1164@naver.com>
2021-02-01 18:36:26 +0900

    팔로우 언팔로우

--- prepare/front/sagas/user.js
+++ prepare/front/sagas/user.js
@@ -1,6 +1,8 @@
 import {all, delay, fork, put, takeLatest} from 'redux-saga/effects';
 import axios from 'axios';
 import {
+  FOLLOW_FAILURE,
+  FOLLOW_SUCCESS,
   LOG_IN_FAILURE,
   LOG_IN_REQUEST,
   LOG_IN_SUCCESS,
@@ -10,10 +12,52 @@ import {
   SIGN_UP_FAILURE,
   SIGN_UP_REQUEST,
   SIGN_UP_SUCCESS,
+  UNFOLLOW_FAILURE,
+  UNFOLLOW_SUCCESS,
 } from '../reducers/user';
 
+function followAPI(data) {
+  return axios.post('/api/follow', data);
+}
+
+function* follow(action) {
+  try {
+    // const result = yield call(logInAPI);
+    yield delay(2000);
+    yield put({
+      type: FOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (e) {
+    yield put({
+      type: FOLLOW_FAILURE,
+      data: e.response.data,
+    });
+  }
+}
+
+function unfollowAPI(data) {
+  return axios.post('/api/unfollow', data);
+}
+
+function* unfollow(action) {
+  try {
+    // const result = yield call(logInAPI);
+    yield delay(2000);
+    yield put({
+      type: UNFOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (e) {
+    yield put({
+      type: UNFOLLOW_FAILURE,
+      data: e.response.data,
+    });
+  }
+}
+
 function logInAPI(data) {
-  return axios.post('/api/login', data)
+  return axios.post('/api/login', data);
 }
 
 function* logIn(action) {
@@ -33,7 +77,7 @@ function* logIn(action) {
 }
 
 function logOutAPI() {
-  return axios.post('/api/logout')
+  return axios.post('/api/logout');
 }
 
 function* logOut() {
@@ -53,7 +97,7 @@ function* logOut() {
 }
 
 function signUpAPI() {
-  return axios.post('/api/signUp')
+  return axios.post('/api/signUp');
 }
 
 function* signUp() {
@@ -72,6 +116,14 @@ function* signUp() {
   }
 }
 
+function* watchFollow() {
+  yield takeLatest(LOG_IN_REQUEST, follow);
+}
+
+function* watchUnfollow() {
+  yield takeLatest(LOG_OUT_REQUEST, unfollow);
+}
+
 function* watchLogIn() {
   yield takeLatest(LOG_IN_REQUEST, logIn);
 }
@@ -86,6 +138,8 @@ function* watchSignUp() {
 
 export default function* userSaga() {
   yield all([
+    fork(watchFollow),
+    fork(watchUnfollow),
     fork(watchLogIn),
     fork(watchLogOut),
     fork(watchSignUp),
