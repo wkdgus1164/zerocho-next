fdb147d655d38fdd95b7baba2a190f99c327292d
Han Jang Hyun <wkdgus1164@naver.com>
2021-02-01 16:37:28 +0900

    faker 적용

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Head from 'next/head';
-
 import {useSelector} from 'react-redux';
+import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
 
 const Profile = () => {
   const me = useSelector(state => state.user.me);
+
+  useEffect(() => {
+    if (!(me && me.id)) Router.push('/');
+  }, [me && me.id]);
+
+  if (!me) return null;
 
   return (
     <>
--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,5 +1,6 @@
 import shortId from 'shortid';
 import produce from 'immer';
+import faker from 'faker';
 
 const initialState = {
   mainPosts: [{
@@ -42,6 +43,27 @@ const initialState = {
   addCommentDone: false,
   addCommentError: null,
 };
+
+initialState.mainPosts = initialState.mainPosts.concat(
+  Array(20).fill().map(() => ({
+    id: shortId.generate(),
+    User: {
+      id: shortId.generate(),
+      nickname: faker.name.findName(),
+    },
+    content: faker.lorem.paragraph(),
+    Images: [{
+      src: faker.image.image(),
+    }],
+    Comments: [{
+      User: {
+        id: shortId.generate(),
+        nickname: faker.name.findName(),
+      },
+      content: faker.lorem.sentence(),
+    }],
+  })),
+);
 
 export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
 export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
